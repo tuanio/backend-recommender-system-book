e036d3207f8aa0a33d62db892bdb5ae487d923c3
tuanio <nvatuan3@gmail.com>
2021-10-27 15:10:05 +0700

    config a little bit

--- app/models.py
+++ app/models.py
@@ -1,16 +1,11 @@
 from app import db
 
- 
 class Author(db.Model):
     __tablename__="author"
     id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String(255),nullable=False)
-    def __init__(self, full_name):
-        self.full_name=full_name
+
     def __repr__(self):
-        return "<Author ({},{})>".format(
-            self.id,
-            self.full_name
-        )
+        return "<Author({}, {})>".format(self.id, self.full_name)
 
 
