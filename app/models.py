bea8d6bd1fe5b0ba606693bb470f06c2de995166
Thinh127 <thinh7174@gmail.com>
2021-11-02 21:31:42 +0700

    commit branch thinh

--- app/models.py
+++ app/models.py
@@ -1,1 +1,8 @@
 from app import db
+
+class Author(db.Model):
+    __tablename__ = "author"
+    id = db.Column(db.Integer, primary_key = True)
+    full_name = db.Column(db.String(255), nullable = True)
+
+    
